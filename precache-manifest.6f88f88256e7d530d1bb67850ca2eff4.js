2c89725d2e5500723108cc4930f0c9ea31a7fa2f
ChengHan <henry32144@hotmail.com>
2021-01-10 18:50:12 +0800

    update placeholder

---
+++ precache-manifest.6f88f88256e7d530d1bb67850ca2eff4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca72f2d1448826f430e09f5f2eedb17a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8782d9d8636f7c731b14",
+    "url": "./static/css/main.4cd7a186.chunk.css"
+  },
+  {
+    "revision": "9a4b411a6d8a6aea1e3d",
+    "url": "./static/js/2.106e0e82.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.106e0e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8782d9d8636f7c731b14",
+    "url": "./static/js/main.5da694c2.chunk.js"
+  },
+  {
+    "revision": "31539150ea78c5cea9db",
+    "url": "./static/js/runtime-main.552ccdce.js"
+  },
+  {
+    "revision": "a563ba33c0025da247e6da22f6a23281",
+    "url": "./static/media/search.a563ba33.svg"
+  },
+  {
+    "revision": "c7faf4a141d521ca19e4cb3b43fc5844",
+    "url": "./static/media/x-circle-fill.c7faf4a1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-instagram-practice/precache-manifest.f0dfa9a7046966baa35063f61cc79cf7.js"
+  "./precache-manifest.6f88f88256e7d530d1bb67850ca2eff4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-instagram-practice/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
