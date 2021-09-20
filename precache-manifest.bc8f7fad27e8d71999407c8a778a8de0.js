81118e2a5354a59a715a80dd8fdf93fb88b4e04f
Sky <chenghanwu32144@gmail.com>
2021-09-20 14:31:44 -0700

    Updates

---
+++ precache-manifest.bc8f7fad27e8d71999407c8a778a8de0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03499f1c9ceaee23e20f9986020565c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5310d090a30686df53e4",
+    "url": "./static/css/main.531936ef.chunk.css"
+  },
+  {
+    "revision": "9a4b411a6d8a6aea1e3d",
+    "url": "./static/js/2.106e0e82.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.106e0e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5310d090a30686df53e4",
+    "url": "./static/js/main.e89e4706.chunk.js"
+  },
+  {
+    "revision": "31539150ea78c5cea9db",
+    "url": "./static/js/runtime-main.552ccdce.js"
+  },
+  {
+    "revision": "a563ba33c0025da247e6da22f6a23281",
+    "url": "./static/media/search.a563ba33.svg"
+  },
+  {
+    "revision": "c7faf4a141d521ca19e4cb3b43fc5844",
+    "url": "./static/media/x-circle-fill.c7faf4a1.svg"
+  }
+]);
